9f7a7c5bf3da725bdb26ab02bf3dfc4a09639720
malen tucuric <malencuturic@gmail.com>
2013-11-24 20:49:19 -0500

    Standalone Executable
    Added functionality for a standalone executable

--- placekitten.py
+++ placekitten.py
@@ -1,17 +1,26 @@
-from urllib2 import urlopen
-import random
-import os
-import ctypes
+from requests import get
+from random import randint
+from os import environ
 
-width = random.randint(200,500)
-height = width-random.randint(1,100)
+if environ["PROCESSOR_ARCHITECTURE"] == "x86":
+    import win32api as winapi,win32con as wincon,win32gui as wingui
+    version = 32
+else:
+    import win64api as winapi, win64con as wincon, win64gui as wingui
 
-url = 'http://placekitten.com/{0}/{1}'.format(str(width),str(height))
-kitten = urlopen(url).read()
+x = randint(300,1000)
+y = x-randint(0,100)
+url = "http://placekitten.com/{0}/{1}".format(x,y)
 
-os.chdir('C:/Users/Public')
-with open('kitten.bmp' , 'wb') as f: f.write(kitten)
-f.close()
+path = "kitten.png"
 
-SPI_SETDESKWALLPAPER = 20
-ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, "kitten.bmp" , 0)
+r = get(url, stream=True)
+if r.status_code == 200:
+    with open(path, 'wb') as f:
+        for chunk in r.iter_content(1024):
+            f.write(chunk)
+
+key = key = winapi.RegOpenKeyEx(wincon.HKEY_CURRENT_USER,"Control Panel\\Desktop",0,wincon.KEY_SET_VALUE)
+winapi.RegSetValueEx(key, "WallpaperStyle", 0, wincon.REG_SZ,"0")
+winapi.RegSetValueEx(key, "TileWallpaper", 0, wincon.REG_SZ,"0")
+wingui.SystemParametersInfo(wincon.SPI_SETDESKWALLPAPER, path, 1+2)
